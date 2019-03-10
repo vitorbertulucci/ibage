a8a1260ae9129082aaeddb286c77b0fc49ad08c2
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2019-03-10 19:43:43 -0300

    Adding texts to trainning page

--- src/pages/Training.vue
+++ src/pages/Training.vue
@@ -15,8 +15,51 @@
           .four
           .five
       .training-page__content
-        h5.title 1.Gerenciamento do Sistema de Informações sobre Orçamento Público em Educação (SIOPE)
+        h5.title 1. Gerenciamento do Sistema de Informações sobre Orçamento Público em Educação (SIOPE)
         p Apresentar aos gestores municipais da área de educação e aos técnicos responsáveis pelo gerenciamento dos recursos como preencher corretamente o Sistema de Informações sobre Orçamento Público em Educação (SIOPE) de forma que as informações venham a auxiliá-los no planejamento de suas ações, fornecendo dados atualizados sobre as receitas públicas e os correspondentes recursos vinculados à educação, assegurando maior transparência na Gestão Educacional por meio dos indicadores gerados pelo Sistema.
+
+        h5.title 2. Entendo o SICONV – Portal de Convênios
+        p Viabilizar a atuação na gestão convênios federais por meio do Portal de Convênios do Governo Federal (SICONV), visando a compreensão do funcionamento do sistema de forma correta. Demonstrar, também, passo a passo todo o processo de captação, formalização, execução e prestação de contas, abordando os principais módulos do SICONV, com ênfase na execução de convênios.
+
+        h5.title 3. Conhecendo o Sistema de Monitoramento de Obras da Saúde (SISMOB)
+        p
+          | Transmitir conhecimentos sobre a operacionalização dos diferentes módulos do Sistema de Monitoramento de Obras da Saúde (SISMOB), relacionado ao Fundo Nacional da Saúde (FNS).
+          br
+          | O Ministério da Saúde apoia financeiramente a execução de obras para construção, reforma e ampliação de estabelecimentos de saúde. As obras podem ser financiadas via contrato de repasse com a Caixa (quando não há projeto de referência) ou por meio do financiamento fundo a fundo (quando há entrega bem definida, com acompanhamento de resultado). No SISMOB são cadastradas e monitoradas as obras com projeto de referência, para as quais o Ministério da Saúde transfere os recursos em parcelas aos estados e municípios.
+
+        h5.title 4. Conhecendo o Sistema Integrado de Monitoramento Execução e Controle (SIMEC)
+        p Possibilitar que os participantes do curso conheçam o Sistema Integrado de Execução Monitoramento e Controle (SIMEC), e iniciem a navegação pelo módulo Plano de Ações Articuladas (PAR), permitindo a compreensão e correta utilização do sistema na fase preparatória.
+
+        h5.title 5. Entendendo o Sistema de Informação sobre Orçamento Público em Saúde (SIOPS)
+        p
+          | Capacitar os servidores e agentes públicos da esfera municipal e estadual de acordo com os conceitos ligados ao Sistema de Informações sobre Orçamentos Públicos em Saúde (SIOPS) e todas as alterações que ocorreram. Pretende-se, ainda, demonstrar a prática correta através do sistema para dar segurança no preenchimento das diversas pastas e dados requeridos pelo sistema, de forma a gerar corretos demonstrativos com despesas de saúde.
+          br
+          | Auxiliar, também, os gestores e servidores mostrando os impactos na gestão municipal da Política Pública de Saúde previstos na Portaria 3.992/2017.
+
+        h5.title 6. Sistema de Gestão de Prestação de Contas (SIGPC) do FNDE
+        p Capacitar os diversos usuários do Sistema de Gestão de Prestação de Contas (SIGPC) para acompanhamento e correta apresentação das prestações de contas de recursos transferidos aos Estados, Municípios e Distrito Federal, no âmbito dos programas PNAE, PNAT e PDDE, do Fundo Nacional de Desenvolvimento da Educação (FNDE). Apresentar as principais novidades do sistema. Demonstrar na prática as funcionalidades e utilização disponibilizados no SIGPC e repasse das informações sobre o funcionamento e o acompanhamento do CAE através do Sistema de Gestão de Conselho (SIGECON).
+
+        h5.title 7. Entendendo a nova Legislação de Convênios e Contratos de Repasse
+        p Proporcionar aos participantes conhecimento prático e objetivo na nova legislação de convênios e contratos de repasses públicos, envolvendo todas as fases atos preparatórios, celebração, execução, fiscalização, prestação de contas e tomada de contas especial.
+
+        h5.title 8. Descomplicando o Termo de Execução Descentralizada (TED)
+        p Possibilitar aos participantes o conhecimento das formas de qualificação, apresentação formalização, execução e fiscalização de termos de execução descentralizada, de forma a garantir a correta aplicação da Lei e das normas aplicáveis à matéria.
+
+        h5.title 9. Entendendo sobre Licitações e Contratos
+        p Qualificar os profissionais envolvidos com as compras governamentais de maneira a conhecer as regras de licitação na forma da Lei n° 8.666/1993 (e alterações), as fases internas e externas da licitação, as dispensas e inexigibilidades, a modalidade de pregão (presencial e eletrônico) – a luz da Lei nº 10.520/2002, o tratamento diferenciado às micro e pequenas empresas, bem como ter noções sobre o Sistema de Registro de Preços.
+
+        h5.title 10. Como adaptar sua Entidade Sem Fins Lucrativos (ONG) para receber recursos públicos para projetos sociais
+        p Apresentar como as Entidades Sem Fins Lucrativos (ONG) podem se adaptar para receber recursos públicos Federais, Estaduais e Municipais para projetos sociais e como se beneficiarem de regras simplificadas de prestação de contas de transferências de até seiscentos mil reais (R$ 600.000,00). Identificar novos instrumentos viabilizados pelo “Marco Regulatório do Terceiro Setor”, Lei nº 13.019/2014, identificando as mudanças e evolução.
+
+        h5.title 11. Entendendo o Marco Regulatório das Organizações da Sociedade Civil (OSCs)
+        p Proporcionar entendimento sobre a gestão de parcerias entre o Poder Público e as Organizações da Sociedade Civil (OSCs), considerando a Lei Federal nº 13.019/2014, apresentando todas as etapas dessas parcerias, em relação à elaboração do plano de trabalho e a prestação de contas.
+
+        h5.title 12. Como elaborar um Termo de Referência e um Projeto Básico
+        p Capacitar os participantes a preparar especificações técnicas, projetos básicos e termos de referência que assegurem a celebração de contratos administrativos e a realização de processos de licitações econômicos e juridicamente eficientes, assim como ao entendimento do processo de contratações no setor público.
+
+        h5.title 13. Parcerias Público-Privadas (PPP’s) e Concessões
+        p Capacitar e atualizar os servidores e demais colaboradores acerca das normas gerais para licitação e contratação de parceria público-privadas no âmbito da Administração Pública, de forma a resguardar a lisura e adequação dos procedimentos licitatórios e gestão das parcerias público-privadas. Fornecer fundamentação jurídica para a adoção das parcerias público-privadas e das concessões de serviços públicos, com abordagem sobre a Lei nº 11.079/2004.
+
 </template>
 
 <script>
@@ -139,4 +182,10 @@ export default {
         display block
         background $primary
         margin-bottom 17px
+
+    p
+      margin-bottom 45px
+
+      &:last-of-type
+        margin-bottom 0
 </style>
